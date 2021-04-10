716b3284f3bf4b1c19290330463eda3b81fa5d04
stephen-le <stephen-le@github.com>
2021-04-10 15:21:37 -0700

    Initial commit

---
+++ src/C-Primer.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : C-Primer.cpp
+// Author      : Stephen
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello C++11!" << endl; // prints Hello C++11!
+	return 0;
+}
